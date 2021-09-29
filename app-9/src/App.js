956fe34cb1000da81f8a2b026085888e4031f83b
Zach King <zacheking10@gmail.com>
2021-09-28 22:32:40 -0600

    app-9 finished

--- app-9/src/App.js
+++ app-9/src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Link } from "react-router-dom"
 import './App.css';
+import Router from './Router';
 
+
+// prettier-ignore
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <nav>
+          <ul style={{ display: "flex", justifyContent: "space-around" }}>
+            <Link to="/">Home</Link>
+            <Link to="/signup">Signup</Link>
+            <Link to="/details">Details</Link>
+          </ul>
+        </nav>
+
+        { Router }
+      </div>
   );
 }
 
